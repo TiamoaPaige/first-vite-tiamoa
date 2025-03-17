70a26c1e0b03b5c242ab946101d646fb6845a058
TiamoaPaige <paigetiamoa2021@icloud.com>
2025-03-17 16:46:21 -0400

    initial commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function About() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <p>This is my about page!</p>
+    </>
+  )
+}
+
+export default About 
